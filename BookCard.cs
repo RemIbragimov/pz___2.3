5043f9c96b2f53e53f1130947affcaa6e6f7235d
Голушко Екатерина <ekaterinagolushko@yandex.ru>
2022-02-10 16:27:06 +0300

    Добавьте файлы проекта.

---
+++ BookCard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pz___2._3
+{
+    class BookCard
+    {
+        public int ID; 
+        public string author;
+        public string title;
+        public int year;
+        public string condition;
+        public BookCard(int _ID, string _author, string _title, int _year, string _condition)
+        {
+            ID = _ID;
+            author = _author;
+            title = _title;
+            year = _year;
+            condition = _condition;
+        }
+
+        public void GetBookInfo()
+        {
+            Console.WriteLine($"ID = {ID}, year = {year}, author = {author}, title = {title}, condition = {condition}");
+        }
+    }
+}
+    
+
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pz___2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookCard b1 = new BookCard(1, "Пушкин", "Дубровский", 1999, "Отличное");
+            BookCard b2 = new BookCard(2, "Лермонтов", "Парус", 2001, "Удовлетворяющие");
+            BookCard b3 = new BookCard(3, "Есенин", "Сорокоуст", 1988, "Неудовлетворяющие");
+            BookCard b4 = new BookCard(4, "Толстой", "Казаки", 1998, "Хорошое");
+
+            b1.GetBookInfo();
+            b2.GetBookInfo();
+            b3.GetBookInfo();
+            b4.GetBookInfo();
+        }
+    }
+}
